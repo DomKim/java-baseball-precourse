8cbaf5a13c4282cb908dc8fcd498e4ee651c2764
hj <hj@relaybrand.com>
2022-09-29 18:16:50 +0900

    feat : application 에서 게임 시작 호출

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,11 @@
 package baseball;
 
+import baseball.domain.Referee;
+
 public class Application {
     public static void main(String[] args) {
 
         // TODO: 프로그램 구현
+        Referee.palyBall();
     }
 }
