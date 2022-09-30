83052d42446b66276673260a891d9d05b96ca3ac
hj <hj@relaybrand.com>
2022-09-30 18:18:13 +0900

    feat : 심판의 게임 호스트와 플레이어의 숫자 들에 대한 비교 기능 추가

--- src/main/java/baseball/domain/Referee.java
+++ src/main/java/baseball/domain/Referee.java
@@ -1,8 +1,24 @@
 package baseball.domain;
 
-public class Referee {
-    public static void palyBall(){
-        GameHost.prepareForGame();
+import baseball.views.InputViews;
+import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Objects;
+
+public class Referee {
+    public static boolean playBall(){
+        PlayGround playGround = PlayGround.prepareForGame();
+        while(!judge(playGround)){
+            playGround.playerRetry();
+        }
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        return true;
+    }
+
+    public static boolean judge(PlayGround playGround){
+        BallCount ballCount = playGround.compareBetweenGameHostAndPlayer();
+        ballCount.toString();
+
+        return ballCount.isStrike();
     }
 }
