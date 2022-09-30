43bef3d534ef7049275f6709794a19017a802427
hj <hj@relaybrand.com>
2022-09-30 18:17:17 +0900

    feat : 베이스볼 해쉬셋을 반환하는 기능 추가

--- src/main/java/baseball/domain/GameHost.java
+++ src/main/java/baseball/domain/GameHost.java
@@ -3,10 +3,19 @@ package baseball.domain;
 import java.util.LinkedHashSet;
 
 public class GameHost {
-    private BaseBalls baseBalls;
-    public static void prepareForGame(){
-        LinkedHashSet<BaseBall> baseBalls = BaseBalls.generateRandomBalls(GameRules.NUMBER_OF_POSITION.getValue());
 
+    private final BaseBalls baseBalls;
 
+    private GameHost(BaseBalls baseBalls) {
+        this.baseBalls = baseBalls;
+    }
+
+    public static GameHost prepareForGame(){
+        BaseBalls baseBalls = BaseBalls.generateRandomBalls(GameRules.NUMBER_OF_POSITION.getValue());
+        return new GameHost(baseBalls);
+    }
+
+    public LinkedHashSet<BaseBall> getBaseBalls() {
+        return baseBalls.getBaseBalls();
     }
 }
