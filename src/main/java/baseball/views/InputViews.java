092a1bf1dfdee6d4553be2c056bad753c7b1cfd3
hj <hj@relaybrand.com>
2022-09-30 18:19:15 +0900

    refactor : 안내 문구 추가 및 메소드 네이밍 변경

--- src/main/java/baseball/views/InputViews.java
+++ src/main/java/baseball/views/InputViews.java
@@ -1,5 +1,33 @@
 package baseball.views;
 
-public class InputViews {
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
+public class InputViews {
+    private static String NUMBER_FORMAT_ERROR_MESSAGE = "숫자 형식을 입력해주세요.";
+
+    public static int getBallNumberByPlayer() {
+        System.out.println("숫자를 입력해주세요");
+        String threeNumbersByPlayer = "0";
+        while(!validateInt(threeNumbersByPlayer)){
+            threeNumbersByPlayer = Console.readLine();
+        }
+        return Integer.parseInt(threeNumbersByPlayer);
+    }
+
+    public static boolean validateInt(String stringValue){
+        try{
+            Integer.parseInt(stringValue);
+
+        }catch(NumberFormatException e){
+            System.out.println(NUMBER_FORMAT_ERROR_MESSAGE);
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean validateMaxNumber(int maxNumber){
+
+        return false;
+    }
 }
